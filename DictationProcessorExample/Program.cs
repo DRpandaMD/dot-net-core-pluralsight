30f3511ce74a820644280c807d448bdf4b3ad2b8
DRpandaMD <michael.zarate.ce@gmail.com>
2018-06-06 11:36:43 -0500

    wrapped up sample app and deployment options

--- DictationProcessorExample/Program.cs
+++ DictationProcessorExample/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
+using System.IO.Compression;
 
 namespace DictationProcessorExample
 {
@@ -26,18 +27,38 @@ namespace DictationProcessorExample
                     var audioFilepath = Path.Combine(subfolder, metadata.File.FileName);
                     // - verify file checksum
                     var md5Checksum = GetCheckSum(audioFilepath);
-                    if (md5Checksum.Replace("-","").ToLower() != metadata.File.Md5Checksum)
+                    if (md5Checksum.Replace("-", "").ToLower() != metadata.File.Md5Checksum)
                     {
                         throw new Exception("checksum not validated! File corrupted");
                     }
                     // - generate a unique identifier
+                    var uniqueID = Guid.NewGuid();
+                    metadata.File.FileName = uniqueID + ".WAV";
+                    var newpath = Path.Combine("../Data/ready_for_transcription", uniqueID + ".WAV");
                     // - compress it
+                    CreateCompressedFile(audioFilepath, newpath);
                     // - create a standalone metadata file
+                    SaveSingleMetadata(metadata, newpath + ".json");
                 }
                 
             }
 
             
+        }
+
+        static void CreateCompressedFile(string inputFilePath, string outputFilePath)
+        {   
+            // add '.gz' to reflect the compressed file
+            outputFilePath += ".gz"; 
+            // console output to track status
+            Console.WriteLine($"Creating {outputFilePath}");
+
+            // create our IO file streams
+            var inputFileStream = File.Open(inputFilePath, FileMode.Open);
+            var outputFileStream = File.Create(outputFilePath);
+            var gzipStream = new GZipStream(outputFileStream, CompressionLevel.Optimal);
+            inputFileStream.CopyTo(gzipStream);
+
         }
 
         static string GetCheckSum(string filePath)
@@ -62,6 +83,22 @@ namespace DictationProcessorExample
                 var serializer = new DataContractJsonSerializer(typeof(List<Metadata>), settings);
                 // here we will call our serializer object to read our filestream and cast it to our list
                  return (List<Metadata>)serializer.ReadObject(metadataFileStream);
+        }
+
+        static void SaveSingleMetadata(Metadata metadata, string metadataFilePath)
+        {
+            // extract metadata, including audio info, from metadata file
+                // -- open file stream
+                Console.WriteLine($"Writing new metadata {metadataFilePath}");
+                var metadataFileStream = File.Open(metadataFilePath, FileMode.Create);
+                var settings = new DataContractJsonSerializerSettings
+                {
+                    DateTimeFormat = new DateTimeFormat("yyyy-MM-dd'T'HH:mm:ssz")
+                };
+                // -- set up serializer
+                var serializer = new DataContractJsonSerializer(typeof(Metadata), settings);
+                // here we will call our serializer object to read our filestream and cast it to our list
+                serializer.WriteObject(metadataFileStream, metadata);
         }
     }
 }
